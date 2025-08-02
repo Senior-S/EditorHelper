5f62ecc1703580036e07939680f184a5940d08cf
Senior-S <maximatador23@gmail.com>
2025-08-01 21:43:52 -0300

    feat: max culling distance changed to level size.

---
+++ EditorHelper/Patches/CullingVolumePatches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using SDG.Framework.Devkit;
+using SDG.Unturned;
+using UnityEngine;
+
+namespace EditorHelper.Patches;
+
+[HarmonyPatch]
+public class CullingVolumePatches
+{
+    [HarmonyPatch(typeof(CullingVolume.Menu), "OnDistanceChanged")]
+    [HarmonyPrefix]
+    [UsedImplicitly]
+    static bool OnDistanceChanged(CullingVolume.Menu __instance, ISleekFloat32Field field, float value)
+    {
+        __instance.volume.cullDistance = Mathf.Clamp(value, 1f, SDG.Unturned.Level.size);
+        __instance.distanceField.Value = __instance.volume.cullDistance;
+        LevelHierarchy.MarkDirty();
+        return false;
+    }
+}
