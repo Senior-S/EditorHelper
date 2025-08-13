ca4333938b224ea5020c574437e6347adb05c9ff
Sultan . <156057376+JienSultan@users.noreply.github.com>
2025-08-13 21:19:30 +0200

    Fixed stuff

--- EditorHelper/Patches/Menu/UI/MenuWorkshopSubmitUIPatches.cs
+++ EditorHelper/Patches/Menu/UI/MenuWorkshopSubmitUIPatches.cs
@@ -5,103 +5,93 @@ using JetBrains.Annotations;
 using SDG.Unturned;
 using UnityEngine;
 
-namespace EditorHelper.Patches.Menu.UI
+namespace EditorHelper.Patches.Menu.UI;
+
+[HarmonyPatch]
+public class MenuWorkshopSubmitUIPatches
 {
-    [HarmonyPatch(typeof(MenuWorkshopSubmitUI))]
-    public class MenuWorkshopSubmitUIPatches
+    private static Local? _localization;
+
+    [HarmonyPatch(typeof(MenuWorkshopSubmitUI), MethodType.Constructor)]
+    [HarmonyPostfix]
+    [UsedImplicitly]
+    private static void Constructor(MenuWorkshopSubmitUI __instance)
     {
-        private static Local? _localization;
+        _localization = Localization.read("/Menu/Workshop/MenuWorkshopSubmit.dat");
+        SleekButtonState typeState = MenuWorkshopSubmitUI.typeState;
 
-        [HarmonyPatch(MethodType.Constructor)]
-        [HarmonyPostfix]
-        [UsedImplicitly]
-        private static void ConstructorPostfix(MenuWorkshopSubmitUI instance)
+        SleekButtonState newTypeState = new SleekButtonState(
+            new GUIContent(_localization.format("Map")),
+            new GUIContent(_localization.format("Localization")),
+            new GUIContent(_localization.format("Object")),
+            new GUIContent(_localization.format("Item")),
+            new GUIContent(_localization.format("Vehicle")),
+            new GUIContent(_localization.format("Skin")),
+            new GUIContent(_localization.format("ServerCuration")),
+            new GUIContent("Barn")
+        )
         {
-            _localization = Localization.read("/Menu/Workshop/MenuWorkshopSubmit.dat");
+            PositionOffset_X = typeState.PositionOffset_X,
+            PositionOffset_Y = typeState.PositionOffset_Y,
+            PositionScale_X = typeState.PositionScale_X,
+            SizeOffset_X = typeState.SizeOffset_X,
+            SizeOffset_Y = typeState.SizeOffset_Y,
+            onSwappedState = typeState.onSwappedState
+        };
 
-            SleekButtonState? typeState = Traverse.Create(instance).Field("typeState").GetValue<SleekButtonState>();
-            if (typeState == null)
-            {
-                UnturnedLog.error("Failed to access typeState field in MenuWorkshopSubmitUI");
-                return;
-            }
-
-            var newTypeState = new SleekButtonState(
-                new GUIContent(_localization.format("Map")),
-                new GUIContent(_localization.format("Localization")),
-                new GUIContent(_localization.format("Object")),
-                new GUIContent(_localization.format("Item")),
-                new GUIContent(_localization.format("Vehicle")),
-                new GUIContent(_localization.format("Skin")),
-                new GUIContent(_localization.format("ServerCuration")),
-                new GUIContent("Barn")
-            )
-            {
-                PositionOffset_X = typeState.PositionOffset_X,
-                PositionOffset_Y = typeState.PositionOffset_Y,
-                PositionScale_X = typeState.PositionScale_X,
-                SizeOffset_X = typeState.SizeOffset_X,
-                SizeOffset_Y = typeState.SizeOffset_Y,
-                onSwappedState = typeState.onSwappedState
-            };
-
-            ISleekElement? parent = typeState.Parent;
-            if (parent != null)
-            {
-                parent.RemoveChild(typeState);
-                parent.AddChild(newTypeState);
-            }
-            Traverse.Create(instance).Field("typeState").SetValue(newTypeState);
+        ISleekElement? parent = typeState.Parent;
+        if (parent != null)
+        {
+            parent.RemoveChild(typeState);
+            parent.AddChild(newTypeState);
         }
 
-        [HarmonyPatch("GetSubmissionTags")]
-        [HarmonyPrefix]
-        [UsedImplicitly]
-        private static bool GetSubmissionTags(ref List<string> result, MenuWorkshopSubmitUI instance)
+        MenuWorkshopSubmitUI.typeState = newTypeState;
+    }
+    
+    [HarmonyPatch(typeof(MenuWorkshopSubmitUI), "GetSubmissionTags")]
+    [HarmonyPrefix]
+    [UsedImplicitly]
+    private static bool GetSubmissionTags(ref List<string> __result)
+    {
+        SleekButtonState typeState = MenuWorkshopSubmitUI.typeState;
+
+        List<string> list = new List<string>();
+        switch (typeState.state)
         {
-            SleekButtonState? typeState = MenuWorkshopSubmitUI.typeState;
-
-            var states = typeState.states;
-            UnturnedLog.info($"typeState options: {string.Join(", ", Array.ConvertAll(states, s => s.text))}");
-            UnturnedLog.info($"Current typeState.state: {typeState.state}");
-
-            List<string> list = new List<string>();
-            switch (typeState.state)
-            {
-                case 0:
-                    list.Add("Map");
-                    break;
-                case 1:
-                    list.Add("Localization");
-                    break;
-                case 2:
-                    list.Add("Object");
-                    break;
-                case 3:
-                    list.Add("Item");
-                    break;
-                case 4:
-                    list.Add("Vehicle");
-                    break;
-                case 5:
-                    list.Add("Skin");
-                    break;
-                case 6:
-                    list.Add("Server Curation");
-                    break;
-                case 7:
-                    list.Add("Barn");
-                    break;
-            }
-
-            string? extraTag = MenuWorkshopSubmitUI.ExtraTag;
-            if (!string.IsNullOrEmpty(extraTag))
-            {
-                list.Add(extraTag);
-            }
-
-            result = list;
-            return false;
+            case 0:
+                list.Add("Map");
+                break;
+            case 1:
+                list.Add("Localization");
+                break;
+            case 2:
+                list.Add("Object");
+                break;
+            case 3:
+                list.Add("Item");
+                break;
+            case 4:
+                list.Add("Vehicle");
+                break;
+            case 5:
+                list.Add("Skin");
+                break;
+            case 6:
+                list.Add("Server Curation");
+                break;
+            case 7:
+                list.Add("Barn");
+                break;
         }
+
+        string? extraTag = MenuWorkshopSubmitUI.ExtraTag;
+        if (!string.IsNullOrEmpty(extraTag))
+        {
+            list.Add(extraTag);
+        }
+
+        __result = list;
+        return false;
     }
 }
