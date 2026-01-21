a0be10a76f791843628c443e9957a241bbeb9a98
Gamingtoday093 <Gamingtoday093@gmail.com>
2026-01-21 16:45:39 +0100

    Reworked MapResolutionExtensions UI to be more Friendly

---
+++ EditorHelper2.common/Types/MapResolution.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace EditorHelper2.common.Types;
+public struct MapResolution(uint width, uint height)
+{
+    /// <summary>
+    /// Value of 0 is <see cref="null"/> as it's not a valid dimension
+    /// </summary>
+    public uint Width = width, Height = height;
+
+    public readonly uint GetAspectWidth(MapResolution aspectResolution) => (uint)Mathf.CeilToInt(Height * ((float)aspectResolution.Width / (float)aspectResolution.Height));
+    public readonly uint GetAspectHeight(MapResolution aspectResolution) => (uint)Mathf.CeilToInt(Width * ((float)aspectResolution.Height / (float)aspectResolution.Width));
+}
